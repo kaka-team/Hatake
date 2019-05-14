ce932fd57717ff774b55408bb66573b3e7482af8
hatake <838149033@qq.com>
2019-05-14 18:07:45 +0800

    update

---
+++ src/main/java/boot/spring/mq/Consumer.java
@@ -0,0 +1,24 @@
+package boot.spring.mq;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.amqp.rabbit.annotation.RabbitHandler;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Component;
+
+/**
+ *@Description TODO
+ *@Author Simida
+ **/
+
+@Component
+@RabbitListener(queues = RabbitConfig.QUEUE_A)
+public class Consumer {
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    @RabbitHandler
+    public void process(String content) {
+        logger.info("one:接收处理队列A当中的消息： " + content);
+    }
+
+}
---
+++ src/main/java/boot/spring/mq/RabbitConfig.java
@@ -0,0 +1,155 @@
+package boot.spring.mq;
+
+import com.rabbitmq.client.Channel;
+import org.slf4j.LoggerFactory;
+import org.springframework.amqp.core.*;
+import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.ChannelAwareMessageListener;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.rabbit.listener.SimpleMessageListenerContainer;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.beans.factory.config.ConfigurableBeanFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
+
+
+/**
+ * 2 * @Author: ly
+ * 3 * @Date: 2019/5/7 下午4:34
+ * 4
+ */
+
+@Configuration
+public class RabbitConfig {
+
+    private final org.slf4j.Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    @Value("${spring.rabbitmq.host}")
+    private String host;
+
+    @Value("${spring.rabbitmq.port}")
+    private int port;
+
+    @Value("${spring.rabbitmq.username}")
+    private String username;
+
+    @Value("${spring.rabbitmq.password}")
+    private String password;
+
+
+    public static final String EXCHANGE_A = "my-mq-exchange_A";
+    public static final String EXCHANGE_B = "my-mq-exchange_B";
+    public static final String EXCHANGE_C = "my-mq-exchange_C";
+    public static final String FANOUT_EXCHANGE = "fanoutExchange";
+
+    public static final String TOP_EXCHANGE = "topExchange";
+
+
+    public static final String QUEUE_A = "QUEUE_A";
+    public static final String QUEUE_B = "QUEUE_B";
+    public static final String QUEUE_C = "QUEUE_C";
+
+    public static final String ROUTINGKEY_A = "spring-boot-routingKey_A";
+    public static final String ROUTINGKEY_B = "spring-boot-routingKey_B";
+    public static final String ROUTINGKEY_C = "spring-boot-routingKey_C";
+
+    @Bean
+    public ConnectionFactory connectionFactory() {
+        CachingConnectionFactory connectionFactory = new CachingConnectionFactory(host,port);
+        connectionFactory.setUsername(username);
+        connectionFactory.setPassword(password);
+        connectionFactory.setVirtualHost("/");
+        connectionFactory.setPublisherConfirms(true);
+        return connectionFactory;
+    }
+
+    @Bean
+    @Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE)
+    //必须是prototype类型
+    public RabbitTemplate rabbitTemplate() {
+        RabbitTemplate template = new RabbitTemplate(connectionFactory());
+        return template;
+    }
+    @Bean
+    public DirectExchange defaultExchange() {
+        return new DirectExchange(EXCHANGE_A);
+    }
+    @Bean
+    public Queue queueA() {
+        return new Queue(QUEUE_A, true); //队列持久
+    }
+    @Bean
+    public Queue queueB() {
+        return new Queue(QUEUE_B, true); //队列持久
+    }
+    @Bean
+    public Queue queueC() {
+        return new Queue(QUEUE_C, true); //队列持久
+    }
+    @Bean
+    public Binding binding() {
+        return BindingBuilder.bind(queueA()).to(defaultExchange()).with(RabbitConfig.ROUTINGKEY_A);
+    }
+    @Bean
+    public Binding bindingB(){
+        return BindingBuilder.bind(queueB()).to(defaultExchange()).with(RabbitConfig.ROUTINGKEY_B);
+    }
+
+    @Bean
+    public SimpleMessageListenerContainer messageContainer() {
+        //加载处理消息A的队列
+        SimpleMessageListenerContainer container = new SimpleMessageListenerContainer(connectionFactory());
+        //设置接收多个队列里面的消息，这里设置接收队列A
+        //假如想一个消费者处理多个队列里面的信息可以如下设置：
+        container.setQueues(queueA(),queueB(),queueC());
+        //container.setQueues(queueA());
+        container.setExposeListenerChannel(true);
+        //设置最大的并发的消费者数量
+        container.setMaxConcurrentConsumers(10);
+        //最小的并发消费者的数量
+        container.setConcurrentConsumers(1);
+        //设置确认模式手工确认
+        container.setAcknowledgeMode(AcknowledgeMode.MANUAL);
+        container.setMessageListener(new ChannelAwareMessageListener() {
+            @Override
+            public void onMessage(Message message, Channel channel) throws Exception {
+                /**通过basic.qos方法设置prefetch_count=1，这样RabbitMQ就会使得每个Consumer在同一个时间点最多处理一个Message，
+                 换句话说,在接收到该Consumer的ack前,它不会将新的Message分发给它 */
+                channel.basicQos(1);
+                byte[] body = message.getBody();
+                logger.info("接收处理队列"+"当中的消息123:" + new String(body));
+                /**为了保证永远不会丢失消息，RabbitMQ支持消息应答机制。
+                 当消费者接收到消息并完成任务后会往RabbitMQ服务器发送一条确认的命令，然后RabbitMQ才会将消息删除。*/
+                channel.basicAck(message.getMessageProperties().getDeliveryTag(), false);
+            }
+        });
+        return container;
+    }
+    //配置fanout_exchange
+    @Bean
+    TopicExchange topicExchange() {
+        return new TopicExchange(RabbitConfig.TOP_EXCHANGE);
+    }
+    @Bean
+    FanoutExchange fanoutExchange() {
+        return new FanoutExchange(RabbitConfig.FANOUT_EXCHANGE);
+    }
+
+    //把所有的队列都绑定到这个交换机上去
+    @Bean
+    Binding bindingExchangeA(Queue queueA,TopicExchange topicExchange) {
+        return BindingBuilder.bind(queueA).to(topicExchange).with("topic.*");
+    }
+    @Bean
+    Binding bindingExchangeB(Queue queueB, TopicExchange topicExchange) {
+        return BindingBuilder.bind(queueB).to(topicExchange).with("topic.#");
+    }
+    @Bean
+    Binding bindingExchangeC(Queue queueC, TopicExchange topicExchange) {
+        return BindingBuilder.bind(queueC).to(topicExchange).with("*.topic");
+    }
+
+
+}
