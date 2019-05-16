acb567902159a4e306c40f866fad2380e2591a28
hatake <838149033@qq.com>
2019-05-16 14:33:39 +0800

    update

--- src/main/java/boot/spring/mq/Consumer.java
+++ src/main/java/boot/spring/mq/Consumer.java
@@ -1,10 +1,13 @@
 package boot.spring.mq;
 
+import boot.spring.dao.ActorDao;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.amqp.rabbit.annotation.RabbitHandler;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+
 
 /**
  *@Description TODO
@@ -15,7 +18,8 @@ import org.springframework.stereotype.Component;
 @RabbitListener(queues = RabbitConfig.QUEUE_A)
 public class Consumer {
     private final Logger logger = LoggerFactory.getLogger(this.getClass());
-
+    @Autowired
+    private ActorDao actorDao;
     @RabbitHandler
     public void process(String content) {
         logger.info("one:接收处理队列A当中的消息： " + content);
--- src/main/java/boot/spring/mq/Consumer2.java
+++ src/main/java/boot/spring/mq/Consumer2.java
@@ -1,10 +1,19 @@
 package boot.spring.mq;
 
+import boot.spring.dao.ActorDao;
+import com.rabbitmq.client.Channel;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.amqp.rabbit.annotation.RabbitHandler;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.amqp.support.AmqpHeaders;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.messaging.handler.annotation.Headers;
 import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+import java.util.Map;
+
 
 /**
  *@Description TODO
@@ -15,10 +24,26 @@ import org.springframework.stereotype.Component;
 @RabbitListener(queues = RabbitConfig.QUEUE_A)
 public class Consumer2 {
     private final Logger logger = LoggerFactory.getLogger(this.getClass());
-
+    @Autowired
+    private ActorDao actorDao;
     @RabbitHandler
-    public void process(String content) {
-        logger.info("two:接收处理队列A当中的消息： " + content);
+    public void process(String message, Channel channel, @Headers Map<String,Object> map) {
+        logger.info("two:接收处理队列A当中的消息： " + message);
+        System.out.println(message);
+        if (map.get("error")!= null){
+            System.out.println("错误的消息");
+            try {
+                channel.basicNack((Long)map.get(AmqpHeaders.DELIVERY_TAG),false,true);      //否认消息
+                return;
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        try {
+            channel.basicAck((Long)map.get(AmqpHeaders.DELIVERY_TAG),false);            //确认消息
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
 }
