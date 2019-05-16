54a9b28406aa28a05c982f8d312555d66353a989
hatake <838149033@qq.com>
2019-05-16 14:33:48 +0800

    update

--- src/main/java/boot/spring/mq/RabbitConfig.java
+++ src/main/java/boot/spring/mq/RabbitConfig.java
@@ -1,13 +1,10 @@
 package boot.spring.mq;
 
-import com.rabbitmq.client.Channel;
 import org.slf4j.LoggerFactory;
 import org.springframework.amqp.core.*;
 import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
-import org.springframework.amqp.rabbit.core.ChannelAwareMessageListener;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
-import org.springframework.amqp.rabbit.listener.SimpleMessageListenerContainer;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.beans.factory.config.ConfigurableBeanFactory;
 import org.springframework.context.annotation.Bean;
@@ -92,12 +89,8 @@ public class RabbitConfig {
     public Binding binding() {
         return BindingBuilder.bind(queueA()).to(defaultExchange()).with(RabbitConfig.ROUTINGKEY_A);
     }
-    @Bean
-    public Binding bindingB(){
-        return BindingBuilder.bind(queueB()).to(defaultExchange()).with(RabbitConfig.ROUTINGKEY_B);
-    }
-
-    @Bean
+    /*这就是一个消费者*/
+   /* @Bean
     public SimpleMessageListenerContainer messageContainer() {
         //加载处理消息A的队列
         SimpleMessageListenerContainer container = new SimpleMessageListenerContainer(connectionFactory());
@@ -115,18 +108,19 @@ public class RabbitConfig {
         container.setMessageListener(new ChannelAwareMessageListener() {
             @Override
             public void onMessage(Message message, Channel channel) throws Exception {
-                /**通过basic.qos方法设置prefetch_count=1，这样RabbitMQ就会使得每个Consumer在同一个时间点最多处理一个Message，
-                 换句话说,在接收到该Consumer的ack前,它不会将新的Message分发给它 */
-                channel.basicQos(1);
+                *//**通过basic.qos方法设置prefetch_count=1，这样RabbitMQ就会使得每个Consumer在同一个时间点最多处理一个Message，
+                 换句话说,在接收到该Consumer的ack前,它不会将新的Message分发给它 *//*
+                channel.basicQos(0,1,false);
                 byte[] body = message.getBody();
-                logger.info("接收处理队列"+"当中的消息123:" + new String(body));
-                /**为了保证永远不会丢失消息，RabbitMQ支持消息应答机制。
-                 当消费者接收到消息并完成任务后会往RabbitMQ服务器发送一条确认的命令，然后RabbitMQ才会将消息删除。*/
+                logger.info("接收处理队列"+"当中的消息:" + new String(body));
+                *//**为了保证永远不会丢失消息，RabbitMQ支持消息应答机制。
+                 当消费者接收到消息并完成任务后会往RabbitMQ服务器发送一条确认的命令，然后RabbitMQ才会将消息删除。*//*
                 channel.basicAck(message.getMessageProperties().getDeliveryTag(), false);
+                logger.info(message.getMessageProperties().getDeliveryTag() + "");
             }
         });
         return container;
-    }
+    }*/
     //配置fanout_exchange
     @Bean
     TopicExchange topicExchange() {
