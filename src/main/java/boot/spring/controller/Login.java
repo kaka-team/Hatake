a8f1d81d3aded940f62841a419478b8b34679286
hatake <838149033@qq.com>
2019-05-29 11:08:12 +0800

    测试回退

--- src/main/java/boot/spring/controller/Login.java
+++ src/main/java/boot/spring/controller/Login.java
@@ -1,20 +1,20 @@
 package boot.spring.controller;
 
-import javax.servlet.http.HttpSession;
-
+import boot.spring.service.LoginService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import boot.spring.service.LoginService;
+import javax.servlet.http.HttpSession;
 
 
 @Controller
 public class Login {
 	@Autowired
 	LoginService loginservice;
+
 
 
 	@RequestMapping(value="/loginvalidate",method=RequestMethod.POST)
