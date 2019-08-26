d473efae91c69ea0483dc2d3ad3e2c470a41c0e1
caokun <838149033@qq.com>
2019-08-27 00:05:55 +0800

    2019_08_04:person redis js讲解

--- src/main/java/boot/spring/controller/CityController.java
+++ src/main/java/boot/spring/controller/CityController.java
@@ -22,23 +22,37 @@ public class CityController {
 	@Autowired
 	CityService cityservice;
 	/*@Autowired
-	JedisCluster jedisCluster;
-*/
+	JedisCluster jedisCluster;*/
 	private static final String cityKey = "REDIS_CITY_KEY";
 
 	@RequestMapping("/getcitys")
 	@ResponseBody
 	CityGrid getcitys(@RequestParam("current") int current,@RequestParam("rowCount") int rowCount){
-		//CityGrid c  = (CityGrid)JSON.parse(jedisCluster.get(cityKey));//从缓存中查找key对应的数据
+		/*CityGrid c  = (CityGrid)JSON.parse(jedisCluster.get(cityKey));//先从缓存中查找key对应的数据
+		如果缓存中存在，则直接返回查找到的数据
+		if(c != null){
+			return c;
+		}else{
 			int total=cityservice.getCitylist().size();
+			//否则从数据库查
 			List<City> list=cityservice.getpagecitylist(current,rowCount);
 			CityGrid grid=new CityGrid();
 			grid.setCurrent(current);
 			grid.setRowCount(rowCount);
 			grid.setRows(list);
 			grid.setTotal(total);
-			//jedisCluster.set(cityKey,JSON.toJSONString(grid));
+			//并且将查到的数据加入到REDIS中，这样下次查就可以从缓存中查到数据了
+			jedisCluster.set(cityKey,JSON.toJSONString(grid));
 			return grid;
+		}*/
+		int total=cityservice.getCitylist().size();
+		List<City> list=cityservice.getpagecitylist(current,rowCount);
+		CityGrid grid=new CityGrid();
+		grid.setCurrent(current);
+		grid.setRowCount(rowCount);
+		grid.setRows(list);
+		grid.setTotal(total);
+		return grid;
 
 
 	}
--- src/main/resources/static/js/showactor.js
+++ src/main/resources/static/js/showactor.js
@@ -1,22 +1,25 @@
 /**
- * showactors
+ * showactors 作用就是，把页面静态组件的动态变化事件写在JS里，比如数据的加载，或者是保存啊，删除啊，这些动态的
  */
  $(document).ready(function(){
  var grid =$("#grid-data").bootgrid({
-    ajax:true,
-    url:"/actors",
+	 //拿到这个列表，然后给这个列表定义一个打开这个页面就要去做的事情，这个页面出来的一瞬间就要去干的事情
+     ajax:true,
+	 //所以这里就发一个AJAX请求，去JAVA端获取相应的数据
+     url:"/actors",
      ajaxSettings: {
-        method: "GET",
-        cache: false
-    },
-        formatters: {
-        "commands": function(column, row)
-        {
-            return "<button type=\"button\" class=\"btn btn-xs btn-default command-edit\" data-row-id=\"" + row.actor_id + "\">编辑<span class=\"glyphicon glyphicon-pencil\"></span></button> "
-            +"<button type=\"button\" class=\"btn btn-xs btn-default command-delete\" data-row-id=\'" + row.actor_id + "\'>删除<span class=\"glyphicon glyphicon-trash\"></span></button> ";
-        }
-    	}
-    }).on("loaded.rs.jquery.bootgrid", function()
+         method: "GET",
+         cache: false
+     },
+     formatters: {
+         "commands": function(column, row)
+         {
+             return "<button type=\"button\" class=\"btn btn-xs btn-default command-edit\" data-row-id=\"" + row.actor_id + "\">编辑<span class=\"glyphicon glyphicon-pencil\"></span></button> "
+                 +"<button type=\"button\" class=\"btn btn-xs btn-default command-delete\" data-row-id=\'" + row.actor_id + "\'>删除<span class=\"glyphicon glyphicon-trash\"></span></button> ";
+         }
+     }
+ }).on("loaded.rs.jquery.bootgrid", function()//前面的这个AJAX方法从JAVA端拿到数据后，就要调用这个ON方法，ON方法的作用就是把前面
+ //你拿到的数据，给你塞到这个表格中去，至于ON里面写的东西，是固定语法，不用深究
 	{
     /* Executes after data is loaded and rendered */
 	    grid.find(".command-edit").on("click", function(e)
